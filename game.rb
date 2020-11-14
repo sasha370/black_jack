1713e82872ed477b79a77d354944b8b6fe3cb2d1
Aleksandr Karyakin <kng.sasha@mail.ru>
2020-11-14 14:17:27 +0300

    made main_menu

--- game.rb
+++ game.rb
@@ -12,5 +12,37 @@ class Game
     main_menu
   end
 
+  def main_menu
+    puts 'Выберите, что необходимо сделать '
+    puts '1. Начать новую игру'
+    puts '2. Продолжить игру'
+    puts '3. Посмотреть правила'
+    puts '9. Выйти'
+    input = gets.chomp.to_i
+    case input
+      when 1
+        new_game
+      when 2
+        next_game #TODO проверить, что уже была создана игра
+      when 3
+        show_rules
+      when 9
+        puts ' ♡ ♧ ♢ ♤  Спасибо за игру ♡ ♧ ♢ ♤ '
+        exit
+      else
+        clear_screen
+        puts '** Вам необходимо выбрать пункт меню **'
+        main_menu
+    end
+  end
+
+  def new_game
+    
+  end
+
+  def next_game
+    #TODO
+  end
+
 
 end
--- game_io.rb
+++ game_io.rb
@@ -1,6 +1,8 @@
 module Game_IO
   # модуль отвечает за отрисовку базовых событий( отображение игрального стола, карт, рубашек  и т.п.)
+
   private
+
   def greeting
     puts '♡ ♧ ♢ ♤   BlackJack   ♡ ♧ ♢ ♤'
   end
@@ -14,4 +16,11 @@ module Game_IO
     retry
   end
 
+  def show_rules
+    #TODO
+  end
+
+  def clear_screen
+    system("cls") || system("clear") || puts("\e[H\e[2J")
+  end
 end
